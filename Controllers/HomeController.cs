7df6736333e907bf8e504e83017a8d6faf41eb68
RaKeerS <n.srikar22@gmail.com>
2021-06-24 23:46:40 +0530

    Did a really small implementation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Chat_Messenger.Controllers
 {
     public class HomeController : Controller
     {
+        public List<object> messageBox = new List<object>();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
@@ -28,5 +32,17 @@ namespace Chat_Messenger.Controllers
 
             return View();
         }
+
+        [System.Web.Http.HttpPost]
+        public object sendMessage(messageBody message)
+        {
+            messageBox.Add(message);
+            return messageBox;
+        }
+    }
+
+    public struct messageBody {
+        public string accessToken { get; set; }
+        public string messageString { get; set; }
     }
 }
